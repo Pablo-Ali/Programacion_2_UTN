a71ad40eebc3af5b1b04e96e424a3c400b05535b
Pablo-Ali <pablosharifali@gmail.com>
2025-05-22 08:12:09 -0300

    update: agregados métodos finales

--- ModeloParcialSistemaBiblioteca/src/modeloparcialsistemabiblioteca/ModeloParcialSistemaBiblioteca.java
+++ ModeloParcialSistemaBiblioteca/src/modeloparcialsistemabiblioteca/ModeloParcialSistemaBiblioteca.java
@@ -2,6 +2,7 @@ package modeloparcialsistemabiblioteca;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,7 +14,7 @@ public class ModeloParcialSistemaBiblioteca {
 
     public static void main(String[] args) {
         int opcion;
-        
+
         Scanner console = new Scanner(System.in);
 
         List<Publicacion> listaPublicaciones = new ArrayList<>();
@@ -35,23 +36,23 @@ public class ModeloParcialSistemaBiblioteca {
                 case 1:
                     agregarPublicacion(listaPublicaciones, console);
                     break;
-                
+
                 case 2:
                     mostrarPublicaciones(listaPublicaciones);
                     break;
-                
+
                 case 3:
                     leerPublicaciones(listaPublicaciones);
                     break;
-                
+
                 case 4:
                     ordenarPublicacionesSegunAño(listaPublicaciones);
                     break;
-                
+
                 case 5:
                     ordenarPublicacionesSegunTitulo(listaPublicaciones, new Comparador());
                     break;
-                    
+
                 default:
                     System.out.println("Opción no válida");
             }
@@ -64,12 +65,12 @@ public class ModeloParcialSistemaBiblioteca {
 
     public static void agregarPublicacion(List<Publicacion> listaPublicaciones, Scanner console) {
         int opcion;
-        
+
         String titulo;
         int año;
-        
+
         Publicacion publicacion;
-        
+
         do {
             System.out.println("#####-AGREGAR PUBLICACION-#####");
             System.out.println("1. Agregar libro");
@@ -84,16 +85,16 @@ public class ModeloParcialSistemaBiblioteca {
                     System.out.println("Ingrese el autor");
                     console.nextLine();
                     String autor = console.nextLine();
-                    
+
                     String genero;
-                    do{
+                    do {
                         System.out.println("Ingrese el genero:\nFICCION\nNO_FICCION\nHISTORIA\nCIENCIA");
                         genero = console.nextLine().toUpperCase();
-                    }while(!genero.equals("FICCION") && !genero.equals("NO_FICCION") && !genero.equals("HISTORIA")  && !genero.equals("CIENCIA"));
-                    
+                    } while (!genero.equals("FICCION") && !genero.equals("NO_FICCION") && !genero.equals("HISTORIA") && !genero.equals("CIENCIA"));
+
                     System.out.println("Ingrese el titulo");
                     titulo = console.nextLine();
-                    
+
                     System.out.println("Ingrese el año de publicacion");
                     año = console.nextInt();
 
@@ -105,15 +106,15 @@ public class ModeloParcialSistemaBiblioteca {
                         System.out.println("El libro ya existe");
                     }
                     break;
-                
+
                 case 2:
                     System.out.println("Ingrese el numero de edicion");
                     int nro = console.nextInt();
-                   
+
                     System.out.println("Ingrese el titulo");
                     console.nextLine();
                     titulo = console.nextLine();
-                    
+
                     System.out.println("Ingrese el año de publicacion");
                     año = console.nextInt();
 
@@ -125,22 +126,22 @@ public class ModeloParcialSistemaBiblioteca {
                         System.out.println("La revista ya existe");
                     }
                     break;
-                
+
                 case 3:
                     System.out.println("Ingrese el autor");
                     console.nextLine();
                     String nombre = console.nextLine();
-                    
+
                     System.out.println("Ingrese el ancho");
                     int ancho = console.nextInt();
-                    
+
                     System.out.println("Ingrese el alto");
                     int alto = console.nextInt();
-                    
+
                     System.out.println("Ingrese el titulo");
                     console.nextLine();
                     titulo = console.nextLine();
-                    
+
                     System.out.println("Ingrese el año de publicacion");
                     año = console.nextInt();
 
@@ -151,59 +152,96 @@ public class ModeloParcialSistemaBiblioteca {
                         System.out.println("La ilustracion ya existe");
                     }
                     break;
-                
+
                 default:
                     System.out.println("Opción no válida");
             }
         } while (opcion != 0);
     }
-    
-    public static void mostrarPublicaciones(List<Publicacion> listaPublicaciones){
-        if (listaPublicaciones.isEmpty()){
-            System.out.println("Aun no hay publicaciones");
-        }else{
-           for (int i=0; i < listaPublicaciones.size(); i++){
-               System.out.println(listaPublicaciones.get(i).toString());
-            } 
-        }    
-    }
-    
-    public static void leerPublicaciones(List<Publicacion> listaPublicaciones){
+
+    public static void mostrarPublicaciones(List<Publicacion> listaPublicaciones) {
         if (listaPublicaciones.isEmpty()) {
-            System.out.println("Aún no hay publicaciones para leer.");
-        }else{
-           for(Publicacion p : listaPublicaciones){
-               if (p instanceof Legible){
-                   ((Legible) p).leer();
-                }else{
-                    System.out.println("Esta publicacion no puede leerse porque " + p.getTitulo() + " es una ilustracion");
-                }
-            } 
-        }    
-    }
-    
-    public static void ordenarPublicacionesSegunAño(List<Publicacion> listaPublicaciones){
-        if (listaPublicaciones.isEmpty()){
             System.out.println("Aun no hay publicaciones");
-        }else{
-            Collections.sort(listaPublicaciones);
-            for (int i=0; i < listaPublicaciones.size(); i++){
-               System.out.println(listaPublicaciones.get(i).toString() +
-                       " año: " +
-                       listaPublicaciones.get(i).getAñoPublicacion());
-            } 
+        } else {
+            for (int i = 0; i < listaPublicaciones.size(); i++) {
+                System.out.println(listaPublicaciones.get(i).toString());
+            }
         }
     }
-    
-    public static void ordenarPublicacionesSegunTitulo(List<Publicacion> listaPublicaciones, Comparador comparador){
-        if (listaPublicaciones.isEmpty()){
+
+    public static void leerPublicaciones(List<Publicacion> listaPublicaciones) {
+        if (listaPublicaciones.isEmpty()) {
+            System.out.println("Aún no hay publicaciones para leer.");
+        } else {
+            for (Publicacion p : listaPublicaciones) {
+                if (p instanceof Legible) {
+                    ((Legible) p).leer();
+                } else {
+                    System.out.println("Esta publicacion no puede leerse porque " + p.getTitulo() + " es una ilustracion");
+                }
+            }
+        }
+    }
+
+    public static void ordenarPublicacionesSegunAño(List<Publicacion> listaPublicaciones) {
+        if (listaPublicaciones.isEmpty()) {
             System.out.println("Aun no hay publicaciones");
-        }else{
+        } else {
+            Collections.sort(listaPublicaciones);
+            for (int i = 0; i < listaPublicaciones.size(); i++) {
+                System.out.println(listaPublicaciones.get(i).toString()
+                        + " año: "
+                        + listaPublicaciones.get(i).getAñoPublicacion());
+            }
+        }
+    }
+
+    public static void ordenarPublicacionesSegunTitulo(List<Publicacion> listaPublicaciones, Comparador comparador) {
+        if (listaPublicaciones.isEmpty()) {
+            System.out.println("Aun no hay publicaciones");
+        } else {
             Collections.sort(listaPublicaciones, comparador);
             System.out.println("Publicaciones ordenadas alfabeticametne:");
-            for (Publicacion p : listaPublicaciones){
+            for (Publicacion p : listaPublicaciones) {
                 System.out.println(p.getTitulo());
             }
+        }
+    }
+
+    public static void mostrarIlustracionesOrdenadasPorArea(List<Publicacion> listaPublicaciones) {
+        List<Ilustracion> ilustraciones = new ArrayList<>();
+
+        for (Publicacion p : listaPublicaciones) {
+            if (p instanceof Ilustracion) {
+                ilustraciones.add((Ilustracion) p);
+            }
+        }
+
+        Comparator<Ilustracion> porAreaDesc = new Comparator<Ilustracion>() {
+            @Override
+            public int compare(Ilustracion i1, Ilustracion i2) {
+                double area1 = i1.getAncho() * i1.getAlto();
+                double area2 = i2.getAncho() * i2.getAlto();
+                return Double.compare(area2, area1);
+            }
+        };
+
+        Collections.sort(ilustraciones, porAreaDesc);
+
+        for (Ilustracion i : ilustraciones) {
+            System.out.println(i);
+        }
+    }
+
+    public static void mostrarPrimeraUltima(List<Publicacion> listaPublicaciones) {
+        if (!listaPublicaciones.isEmpty()) {
+            Publicacion masVieja = Collections.min(listaPublicaciones);
+            Publicacion masNueva = Collections.max(listaPublicaciones);
+
+            System.out.println("Publicación más antigua: " + masVieja);
+            System.out.println("Publicación más reciente: " + masNueva);
+        } else {
+            System.out.println("No hay publicaciones para comparar.");
         }
     }
 }
